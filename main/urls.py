6780f4421caf5e5b7221b7f51c8bbe51b8fd7219
Aquib <jawed.aquib42@gmail.com>
2024-05-09 10:40:37 +0530

    update

--- main/urls.py
+++ main/urls.py
@@ -6,14 +6,11 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from main.index_view import home
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("views-auth/", include("rest_framework.urls")),
     path("get-token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh-token", TokenRefreshView.as_view(), name="token_refresh"),
     path("verify-token", TokenVerifyView.as_view(), name="token_verify"),
-    path("", home, name="home"),
     path("api/v1/", include("svc.urls")),
 ]
